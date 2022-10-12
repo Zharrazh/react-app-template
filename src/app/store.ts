ebbf08f1ba07e1a11e01dd7972f942e04f4dfc88
Andrey Zharikov <zharikov.a@uspect.ru>
2022-10-12 14:17:43 +0300

    delete feature counter

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import trainsReducer from "../features/trains/trainsSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     trains: trainsReducer,
   },
 });
